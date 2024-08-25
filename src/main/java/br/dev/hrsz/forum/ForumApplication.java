e27a06f08eecfa46105bee60f750eacc25c8c34e
hrszanini <hrszanini@gmail.com>
2024-08-25 09:49:17 -0300

    init: Inicialização de projeto Spring

---
+++ src/main/java/br/dev/hrsz/forum/ForumApplication.java
@@ -0,0 +1,13 @@
+package br.dev.hrsz.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
